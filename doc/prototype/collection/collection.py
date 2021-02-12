e9af99018c8db1a7a24262b51985f79a7b415550
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-02-12 11:53:03 -0500

    Premier commit #2

---
+++ doc/prototype/collection/collection.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+import tkinter.font as font
+
+
+# Fenêtre principale
+window = tk.Tk()
+window.title("Collection")
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
