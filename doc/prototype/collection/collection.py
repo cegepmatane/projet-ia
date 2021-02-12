992e18022920df21fd0751dc097bc0484c0d7b67
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-02-12 11:54:46 -0500

    Page et son background #2

--- doc/prototype/collection/collection.py
+++ doc/prototype/collection/collection.py
@@ -12,10 +12,21 @@ window.columnconfigure(1, minsize=800, weight=1)
 menu = tk.Frame(window, relief=tk.FLAT, bd=0, background="orange", width=450, height=50)
 menu.pack(side="top", fill="x", expand=0)
 # Bouton accueil
-bt_accueil = tk.Button(menu, text="Accueil", command=ouvrir_accueil, bg="#5b5b5b", fg="#f87e28")
+bt_accueil = tk.Button(menu, text="Accueil", command="", bg="#5b5b5b", fg="#f87e28")
 bt_accueil['font'] = font.Font(size=15)
 bt_accueil.pack(side="left", fill="x", expand=1)
 # Bouton collection
-bt_collection = tk.Button(menu, text="Collection", command=ouvrir_collection, bg="#5b5b5b", fg="#f87e28")
+bt_collection = tk.Button(menu, text="Collection", command="", bg="#5b5b5b", fg="#f87e28")
 bt_collection['font'] = font.Font(size=15)
 bt_collection.pack(side="left", fill="x", expand=1)
+
+# Page
+page = tk.Frame(window, relief=tk.FLAT, bd=0, background="grey")
+page.pack(side="bottom", fill="both", expand=1)
+
+# Image
+image = tk.PhotoImage(file='./image/fleur.png')
+LB_image = tk.Label(page, image=image)
+LB_image.pack()
+
+window.mainloop()
