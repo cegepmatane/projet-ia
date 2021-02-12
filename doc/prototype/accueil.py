5e1734fe59f07c55d1344ca06020f4a33330ab48
Yann ROUBEAU <tilodryyy@gmail.com>
2021-02-12 10:43:37 -0500

    Update accueil.py
    Renommage de variable

--- doc/prototype/accueil.py
+++ doc/prototype/accueil.py
@@ -25,24 +25,24 @@ FR_buttons.pack(side="top", fill="x", expand=0)
 
 """ Création d'un "Frame" pour contenir les images transmises, seulement une image statique actuellement mais plus tard 
 aussi le flux de la caméra"""
-FR_camera = tk.Frame(window, relief=tk.FLAT, bd=0, background="grey")
-FR_camera.pack(side="bottom", fill="both", expand=1)
+FR_image = tk.Frame(window, relief=tk.FLAT, bd=0, background="grey")
+FR_image.pack(side="bottom", fill="both", expand=1)
 
 """ Création d'un "Label" qui contient la photo"""
 image = tk.PhotoImage(file='./Images/fleur.png')
-LB_Camera = tk.Label(FR_camera, image=image)
+LB_Camera = tk.Label(FR_image, image=image)
 LB_Camera.pack()
 
 """ Créations des boutons du menu supérieur, pas encore fonctionnel, le premier servira a aller sur la page accueil
 et le deuxième à aller sur la page collection"""
 
 fontBouton = font.Font(size=15)
-BTN_open = tk.Button(FR_buttons, text="Accueil", command=ouvrir_accueil, bg="#5b5b5b", fg="#f87e28")
-BTN_open['font'] = fontBouton
-BTN_open.pack(side="left", fill="x", expand=1)
+BTN_accueil = tk.Button(FR_buttons, text="Accueil", command=ouvrir_accueil, bg="#5b5b5b", fg="#f87e28")
+BTN_accueil['font'] = fontBouton
+BTN_accueil.pack(side="left", fill="x", expand=1)
 
-BTN_save = tk.Button(FR_buttons, text="Collection", command=ouvrir_collection, bg="#5b5b5b", fg="#f87e28")
-BTN_save['font'] = fontBouton
-BTN_save.pack(side="left", fill="x", expand=1)
+BTN_collection = tk.Button(FR_buttons, text="Collection", command=ouvrir_collection, bg="#5b5b5b", fg="#f87e28")
+BTN_collection['font'] = fontBouton
+BTN_collection.pack(side="left", fill="x", expand=1)
 
 window.mainloop()
