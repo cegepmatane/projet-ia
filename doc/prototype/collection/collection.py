b227c0b0ee0203ebf611def6dd7e3daf9e7b4b6d
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-02-12 20:44:45 -0500

    Refactorisation #2

--- doc/prototype/collection/collection.py
+++ doc/prototype/collection/collection.py
@@ -11,14 +11,14 @@ window.columnconfigure(1, minsize=800, weight=1)
 window.configure(background="grey")
 
 # Menu
-menu = tk.Frame(window, relief=tk.FLAT, bd=0, background="orange", width=450, height=50)
-menu.pack(side="top", fill="x", expand=0)
+fr_menu = tk.Frame(window, relief=tk.FLAT, bd=0, background="orange", width=450, height=50)
+fr_menu.pack(side="top", fill="x", expand=0)
 # Bouton accueil
-bt_accueil = tk.Button(menu, text="Accueil", command="", bg="#5b5b5b", fg="#f87e28")
+bt_accueil = tk.Button(fr_menu, text="Accueil", command="", bg="#5b5b5b", fg="#f87e28")
 bt_accueil['font'] = font.Font(size=15)
 bt_accueil.pack(side="left", fill="x", expand=1)
 # Bouton collection
-bt_collection = tk.Button(menu, text="Collection", command="", bg="#5b5b5b", fg="#f87e28")
+bt_collection = tk.Button(fr_menu, text="Collection", command="", bg="#5b5b5b", fg="#f87e28")
 bt_collection['font'] = font.Font(size=15)
 bt_collection.pack(side="left", fill="x", expand=1)
 
@@ -33,6 +33,7 @@ lb_image = tk.Label(fr_page, image=image)
 lb_image.pack()
 """
 
+# Données simulées
 fleurs = []
 for ligne in range(41):
     image_fleur = Image.open('./image/fleur.png')
