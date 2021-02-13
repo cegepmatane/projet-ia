3174c2278a05bd868560e7f0ef0a045987c47d8d
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2021-02-12 20:35:00 -0500

    Affichage dynamique de la collection #2

--- doc/prototype/collection/collection.py
+++ doc/prototype/collection/collection.py
@@ -1,12 +1,14 @@
+from datetime import date
 import tkinter as tk
 import tkinter.font as font
-
+from PIL import Image, ImageTk
 
 # Fenêtre principale
 window = tk.Tk()
 window.title("Collection")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
+window.configure(background="grey")
 
 # Menu
 menu = tk.Frame(window, relief=tk.FLAT, bd=0, background="orange", width=450, height=50)
@@ -21,12 +23,50 @@ bt_collection['font'] = font.Font(size=15)
 bt_collection.pack(side="left", fill="x", expand=1)
 
 # Page
-page = tk.Frame(window, relief=tk.FLAT, bd=0, background="grey")
-page.pack(side="bottom", fill="both", expand=1)
+fr_page = tk.Frame(window, background="grey")
+fr_page.pack(side="bottom", fill="both", expand=True, padx=20, pady=10)
 
+"""
 # Image
 image = tk.PhotoImage(file='./image/fleur.png')
-LB_image = tk.Label(page, image=image)
-LB_image.pack()
+lb_image = tk.Label(fr_page, image=image)
+lb_image.pack()
+"""
+
+fleurs = []
+for ligne in range(41):
+    image_fleur = Image.open('./image/fleur.png')
+    largeur_de_base = 100
+    pourcent_largeur = (largeur_de_base / float(image_fleur.size[0]))
+    hauteur = int((float(image_fleur.size[1]) * float(pourcent_largeur)))
+    image_fleur = image_fleur.resize((largeur_de_base, hauteur), Image.ANTIALIAS)
+    image_fleur = ImageTk.PhotoImage(image_fleur)
+
+    Object = lambda **kwargs: type("Object", (), kwargs)
+    fleur = Object(nom="nom-fleur", image=image_fleur, date=date.today(),)
+    fleurs.append(fleur)
+
+# Grid
+ligne = 0
+colonne = 0
+for fleur in fleurs:
+    # Conteneur
+    if ligne > 5:
+        colonne = colonne + 1
+        ligne = 0
+    fr_fleur = tk.Frame(fr_page, background="white")
+    fr_fleur.grid(row=colonne, column=ligne, padx=5, pady=5)
+
+    # Nom
+    lb_nom_fleur = tk.Label(fr_fleur, text=fleur.nom, font=(None, 15))
+    lb_nom_fleur.grid(row=0, column=1, padx=5, pady=5)
+    # Image
+    lb_image = tk.Label(fr_fleur, image=image_fleur)
+    lb_image.grid(row=0, rowspan=2, column=0, padx=5, pady=5)
+    # Date
+    lb_date_fleur = tk.Label(fr_fleur, text=fleur.date, font=(None, 9))
+    lb_date_fleur.grid(row=1, column=1, padx=5, pady=5)
+
+    ligne = ligne + 1
 
 window.mainloop()
