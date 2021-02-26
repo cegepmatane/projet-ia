80856213e4249de759b46815376f2c77169989d5
Yann ROUBEAU <tilodryyy@gmail.com>
2021-02-25 21:43:01 -0500

    FEAT Passage de la POCCaméra en multi-threadée #5
    Passage de la POCCaméra en multi-threadée. Le programme affiche désormais la caméra au démarrage et permets à l'utilisateur de séléctionner une photo parmis ses fichiers pour l'afficher à la place du flux vidéo.
    
    TODO : Faire en sorte de pouvoir relancer le flux vidéo.
    TODO : Faire en sorte de pouvoir prendre une capture d'écran du flux vidéo.
    TODO : Merge la POCCaméra avec la nouvelle interface "accueil"

--- myFlower/poc/POCCamera/main.py
+++ myFlower/poc/POCCamera/main.py
@@ -3,46 +3,101 @@ from tkinter.filedialog import askopenfilename, asksaveasfilename
 import cv2
 from PIL import ImageTk
 from PIL import Image
+import random
+from threading import Thread
+from threading import Lock
+
+
+class WebcamVideoStream:
+
+    def __init__(self):
+        self.stream = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+        self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+        (self.grabbed, self.frame) = self.stream.read()
+        self.thread = Thread(target=self.update, args=())
+        self.started = False
+        self.read_lock = Lock()
+
+    def start(self):
+        if self.started:
+            print ("WebcamVideoStream already started")
+            return None
+        self.started = True
+        if self.thread.is_alive():
+            print ("WebcamVideoStream thread still alive")
+        else:
+            print(self.thread.is_alive())
+            self.thread.start()
+        return self
+
+    def update(self):
+        while self.started:
+            (grabbed, frame) = self.stream.read()
+            self.read_lock.acquire()
+            self.grabbed, self.frame = grabbed, frame
+            self.read_lock.release()
+
+    def read(self):
+        self.read_lock.acquire()
+        frame = self.frame.copy()
+        self.read_lock.release()
+        return frame
+
+    def stop(self):
+        self.started = False
+        self.stream.release()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.stream.release()
+
+
+class WebcamVideoRead:
+
+    def __init__(self, wvs):
+        self.wvs = wvs
+        self.started = False
+        self.thread = Thread(target=self.update, args=())
+
+    def start(self):
+        self.started = True
+        self.thread.start()
+
+    def update(self):
+        while self.started:
+            fr = cv2.cvtColor(self.wvs.read(), cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(fr, "RGB")
+            i = ImageTk.PhotoImage(image=img)
+            LB_Camera.configure(image=i)
+            LB_Camera.image = i
+
+    def stop(self):
+        self.started = False
 
 
 def open_file():
+    vr.stop()
+    vs.stop()
     """Open a file for editing."""
     filepath = askopenfilename(
-        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")]
+        filetypes=[("Image File", "*.png"), ("All Files", "*.*")]
     )
     if not filepath:
         return
-    txt_edit.delete(1.0, tk.END)
-    with open(filepath, "r") as input_file:
-        text = input_file.read()
-        txt_edit.insert(tk.END, text)
-    window.title(f"Simple Text Editor - {filepath}")
+    print(filepath)
+
+    print("Locked") if vs.read_lock.locked() else print("Unlocked")
+
+    i = ImageTk.PhotoImage(file=filepath)
+    LB_Camera.configure(image=i)
+    LB_Camera.image = i
 
 
 def save_file():
-    """Save the current file as a new file."""
-    filepath = asksaveasfilename(
-        defaultextension="txt",
-        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
-    )
-    if not filepath:
-        return
-    with open(filepath, "w") as output_file:
-        text = txt_edit.get(1.0, tk.END)
-        output_file.write(text)
-    window.title(f"Simple Text Editor - {filepath}")
+    vs.start()
+    vr.start()
 
-
-def screenshot():
-    """ Fonction qui affiche le feed de la caméra de l'utilisateur dans un "Label" tkinter, cette fonction
-     est récursive, à changer plus tard, éventuellement jouer avec des threads, à voir"""
-    _, frame = video.read()
-    fr = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(fr, "RGB")
-    i = ImageTk.PhotoImage(image=img)
-    LB_Camera.configure(image=i)
-    LB_Camera.image = i
-    LB_Camera.after(1, screenshot)
 
 """ Création de la fenêtre principale de l'UI"""
 window = tk.Tk()
@@ -50,19 +105,17 @@ window.title("Test d'application")
 window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
-txt_edit = tk.Text(window)
-
 """ Création d'un "frame" pour contenir les boutons de navigations """
 FR_buttons = tk.Frame(window, relief=tk.RAISED, bd=2, background="red", width=450, height=50)
 FR_buttons.pack(side="top", fill="x", expand=0)
 
 """ Création d'un "Frame" pour contenir les informations, seulement la caméra actuellement mais plus tard 
 aussi la collection de photo etc ..."""
-FR_camera = tk.Frame(window, relief=tk.FLAT, bd=5, background="green")
+FR_camera = tk.Frame(window, relief=tk.FLAT, bd=5, background="#909090")
 FR_camera.pack(side="bottom", fill="both", expand=1)
 
 """ Création d'un "Label" qui contiendra ensuite le flux de la caméra de l'utilisateur, photo en placeholder"""
-image = tk.PhotoImage(file='images/fleur.png')
+image = tk.PhotoImage(file='./Images/fleur.png')
 LB_Camera = tk.Label(FR_camera, image=image)
 LB_Camera.pack()
 
@@ -75,9 +128,12 @@ BTN_save = tk.Button(FR_buttons, text="Sauvegarder photo", command=save_file)
 BTN_save.pack(side="left", fill="x", expand=1)
 
 """ Lancement de la capture vidéo """
-video = cv2.VideoCapture(0)
 
 """ Lancement de la fonction permettant l'affichage du feed caméra sur le label LB_Camera"""
-screenshot()
+
+vs = WebcamVideoStream().start()
+
+vr = WebcamVideoRead(vs)
+vr.start()
 
 window.mainloop()
